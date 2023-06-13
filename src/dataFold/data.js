eb7b7398586f1d67f2402c9ea955fe843cc60cd1
Nikhil Ganireddy <nikhilganireddy@yahoo.com>
2023-06-13 08:07:09 +0530

    first commit

---
+++ src/dataFold/data.js
@@ -0,0 +1,21 @@
+const Data = [{
+    id: 1, title: "Units", value: "Select units"
+}, {
+    id: 2, title: "Kilometers", value: "kilomts"
+}, {
+    id: 3, title: "Meters", value: "mts"
+}, {
+    id: 4, title: "Centimeters", value: "cmts"
+}, {
+    id: 5, title: "Millimeters", value: "millimts"
+}, {
+    id: 6, title: "Mile", value: "mile"
+}, {
+    id: 7, title: "Yard", value: "yard"
+}, {
+    id: 8, title: "Foot", value: "foot"
+}, {
+    id: 9, title: "Inches", value: "inch"
+},]
+
+export default Data
